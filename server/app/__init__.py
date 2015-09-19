ea293121da272cfc1f54bbf894923bc606be919e
graceyin1218 <graceyin1218@gmail.com>
2015-09-19 17:12:19 -0400

    doesn't connect properly still..

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,3 +1,32 @@
-from flask import Flask
-app=Flask(__name__)
-from app import views
+#will eventually be run from colab-sbx-280.oit.duke.edu
+#port 27017 (I think...? is that the default?)
+
+
+#from flask import Flask
+#from flask.ext.pymongo import PyMongo
+#app=Flask(__name__)
+
+
+from views import *
+from pymongo import MongoClient
+
+client = MongoClient("colab-sbx-280.oit.duke.edu", "27017")
+db = client.hackmit
+
+
+"""
+MONGODB_HOST = "localhost"
+MONGODB_PORT = 27017
+
+app.config["MONGO_HOST"] = "colab-sbx-280.oit.duke.edu"
+app.config["MONGO_PORT"] = 27017
+app.config["MONGO_DBNAME"] = "hackmit"
+mongo = PyMongo(app)
+"""
+
+
+#not sure what this line does, will ignore for now.. O:)
+#app.config.from_object(__name__)
+
+app.run()
+
