bf45bf5fc129736994e760fa5ab4383e6f7ce91f
Bitnami <bitnami@ubuntu14-generic-template-01.oit.duke.edu>
2015-09-20 00:46:44 -0400

    Added hardware toggle route/capability

--- JSserver/app.js
+++ JSserver/app.js
@@ -1,7 +1,8 @@
 var express = require ("express"),
-	app = express(),
-	MongoClient = require("mongodb").MongoClient,
-	http = require("http");
+app = express(),
+MongoClient = require("mongodb").MongoClient,
+http = require("http");
+var request=require('request');
 
 var users;
 
@@ -23,6 +24,19 @@ app.get("/api/users",function(req,res)
 		if (err) throw err;
 		return found;
 		}));
+});
+app.get("/api/hardware/buzz", function(req, res){
+request.post(
+    'https://api.particle.io/v1/devices/1d0026000647343339373536/led?access_token=97bbe3755d84dc951179331beacf98cb93b4b6b0',
+    { form: { 'args': 'toggle' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+	console.log(response);
+	res.send("<b>Toggle Complete</b>");
+    }
+);
 });
 app.get("/api/test",function(req,res)
 {
