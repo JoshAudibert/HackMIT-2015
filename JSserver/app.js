bc7531b081d0859cb364fef13f2ff1936cbccd89
Joshua Audibert <Josh@Joshuas-MacBook-Air-2.local>
2015-09-19 20:43:13 -0400

    Made minor fixes on connecting to db

--- JSserver/app.js
+++ JSserver/app.js
@@ -5,26 +5,23 @@ var express = require ("express"),
 
 var users;
 
-MongoClient.connect("mongodb://localhost:27017", function(err, db)
+MongoClient.connect("mongodb://colab-sbx-280.oit.duke.edu:27017/hackmit?connectTimeoutMS=300000", function(err, db)
 {
 	if (err) throw err;
-	
 	users = db.collection("users");
 });
-
 app.set("port", process.env.PORT || 8000);
 
 
 app.get("/friends/:user", function (req, res)
 {
-	var query = {"ObjectId": user["ObjectId"]};
-
-	users.find(query).toArray(function(err, found)
+	var query = {"_id": req.params.user};
+	res.send(users.find(query).toArray(function(err, found)
 	{
 		if (err) throw err;
 
 		return found;
-	});
+	}));
 });
 
 
