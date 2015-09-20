28a439f9d85a759b8a015cc78ff48a37b03dea97
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2015-09-19 20:35:32 -0400

    new alarm, facebook integration

---
+++ SocialAlarmClock/SocialAlarmClock/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  SocialAlarmClock
+//
+//  Created by Elizabeth Wei on 9/19/15.
+//  Copyright © 2015 Elizabeth Wei. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_Header_h */
