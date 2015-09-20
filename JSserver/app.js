42a4dfeaa07fa7a04b219da03e6c2663e87880cc
Joshua Audibert <Josh@Joshuas-MacBook-Air-2.local>
2015-09-20 08:13:26 -0400

    Reorganization of code

--- JSserver/app.js
+++ JSserver/app.js
@@ -7,6 +7,7 @@ var users;
 
 MongoClient.connect("mongodb://main:main1@ds051543.mongolab.com:51543/hackmit", function(err, db)
 {
+	console.log(err)
 	if (err) throw err;
 	users = db.collection("users");
 	console.log(err);
@@ -17,26 +18,25 @@ MongoClient.connect("mongodb://main:main1@ds051543.mongolab.com:51543/hackmit", 
 app.set("port", process.env.PORT || 8080); 
 
 app.get("/api/hardware/buzz", function(req, res){
-/*
-/api/hardware/buzz
-Physically toggles the lights and buzzing on the Particle Photon hardware device
-*/
-request.post(
-    'https://api.particle.io/v1/devices/1d0026000647343339373536/led?access_token=97bbe3755d84dc951179331beacf98cb93b4b6b0',
-    { form: { 'args': 'toggle' } },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-        }
-	console.log(response);
-	res.send("<b>Toggle Complete</b>");
-    }
-);
+	/*
+	/api/hardware/buzz
+	Physically toggles the lights and buzzing on the Particle Photon hardware device
+	*/
+	request.post(
+	    'https://api.particle.io/v1/devices/1d0026000647343339373536/led?access_token=97bbe3755d84dc951179331beacf98cb93b4b6b0',
+	    { form: { 'args': 'toggle' } },
+	    function (error, response, body) {
+	        if (!error && response.statusCode == 200) {
+	            console.log(body)
+	        }
+		console.log(response);
+		res.send("<b>Toggle Complete</b>");
+	    }
+	);
 });
+
 app.get("/api/test",function(req,res)
 {
-
-	
 	users.find(function(err, cursor)
 	{
 		users.find().toArray(function(error,found){
@@ -46,37 +46,35 @@ app.get("/api/test",function(req,res)
 			res.send(found);
 			return found;
 		})
-	});
-
-
-
-	
+	});	
 });
 
 // all variables in URL are ObjectIds. 
 app.get("/friends/:user", function (req, res)
 {
-	var query = {"_id": req.params.user};
-	res.send(users.find(query).toArray(function(err, found)
+	users.find(function(err, cursor)
 	{
-		if (err) throw err;
-
-		return found;
-	}));
+		var query = {"_id": req.params.user};
+		users.find(query).toArray(function(err, found)
+		{
+			if (err) throw err;
+			console.log("found");
+			console.log(found);
+			res.send(found);
+			return found;
+		});
+	});
 });
 
-
 app.get("/", function (req, res)
-{
-	
-		console.log("hi");
-		res.send("hello");
-
+{	
+	console.log("hi");
+	res.send("hello");
 });
 
 app.get("/alarms/:user", function(req, res)
 {
-	var query = {"_id": user};
+	var query = {"_id": req.params.user};
 	var projection = {"_id": false, "alarms": true};
 
 	users.find(query, projection).toArray(function(err, alarms)
@@ -119,13 +117,6 @@ app.post("/alarms/newAlarm/:user/:year/:month/:day/:hour/:minute", function(req,
 		});
 	});
 });
-
-app.get("/", function(req, res)
-{
-	console.log("hi");
-	res.send("hello");
-});
-
 
 http.createServer(app).listen(app.get("port"), function()
 {
